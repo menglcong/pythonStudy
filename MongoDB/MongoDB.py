ad7f6310aabc9bdaa9334346564cde6f103d0ddd
konglin <konglin@tsign.cn>
2018-07-24 18:41:06 +0800

    @konglin@增加批量删除

--- MongoDB/MongoDB.py
+++ MongoDB/MongoDB.py
@@ -12,21 +12,25 @@ class mongo:
 
 
     #查询
-    def find(self):
-        print(self.table_cliet.find_one())
-        return self.table_cliet.find()
+    def find(self, *args):
+        return self.table_cliet.find(*args)
 
-    #插入
-    def insert(self):
-        self.table_cliet.insert_many()
+    #插入一条
+    def insert_one(self, data):
+        self.table_cliet.insert_one(data)
 
-    #删除
-    def delete(self):
-        self.table_cliet.delete_many()
+    #插入多条
+    def insert_many(self, data):
+        self.table_cliet.insert_many(data)
 
     #更新
     def update(self):
         self.table_cliet.update_many()
+
+    #删除
+    def delete(self, *args):
+        self.table_cliet.delete_many(*args)
+
 
     #删除集合
     def drop(self):
@@ -35,6 +39,7 @@ class mongo:
 if __name__ == "__main__":
     test = mongo()
     test.connect("mongodb://localhost:27017/","car","tianjin")
-    data = test.find()
+    myquery = { "city_name": "tianjin" }
+    data = test.find(myquery)
     for x in data:
         print(x)
